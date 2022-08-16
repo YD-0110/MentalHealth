9d26a562b2e866c279d0bcf4c5a38a5e5718c0d6
YD-0110 <90946473+YD-0110@users.noreply.github.com>
2022-08-16 19:32:49 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function question(){
+    window.location = "question.html";
+  }
+
+  function home(){
+    window.location = "index.html";
+}
+
+function istory(){
+    window.location = "istory.html";
+}
+
+function ss(){
+    window.location = "ss.html";
+}
